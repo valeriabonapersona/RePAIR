7b40902d3d25569014a90137eaa6a727ceb6a062
valeriabonapersona <valeria.bonapersona@gmail.com>
2019-07-23 15:40:15 +0200

    written better

--- power_literature.R
+++ power_literature.R
@@ -12,7 +12,7 @@ source("RePAIR_functions.R")
 # osf_auth(OSF_PAT)
 # project <- osf_retrieve_node("wvs7m")
 # 
-# datasets <- c("meta_n.csv", "meta_effectsie.csv")
+# datasets <- c("meta_n.csv", "meta_effectsize.csv")
 # 
 # # Download
 # for (d in datasets) {
@@ -32,7 +32,7 @@ meta <- read.csv("meta_effectsize.csv")
 # Achieved power ----------------------------------------------------------
 # Calculate power achieved for each effect size
 power <- pwr.t2n.test(n1 = meta$n_1, n2 = meta$n_2, 
-                      d = meta$yi, sig.level = .05)
+                      d = meta$yi, sig.level = .05) ## ask Herbert
 meta$power <- power$power
 median(meta$power)
 
@@ -107,9 +107,7 @@ effect_sizes <-
 # Calculation
 dat_theor <- dat %>% 
   bind_rows(dat,dat) %>%
-  mutate(eff_size = c(rep(0.2, nrow(dat)),
-                      rep(0.5, nrow(dat)),
-                      rep(0.9, nrow(dat))))
+  mutate(eff_size = rep(all_es, each = nrow(dat)))
 
 pow <- pwr.t2n.test(n1 = dat_theor$n_1,
                     n2 = dat_theor$n_2,
@@ -120,6 +118,11 @@ dat_theor$theor_pow <- pow$power
 
 
 # theoretical power visualization
+dat_theor %>%
+  group_by(eff_size) %>%
+  summarize(low_per = sum((theor_pow <= 0.5)) /nrow(dat) * 100,
+            high_per = sum((theor_pow >= 0.8))/ nrow(dat) * 100) -> per_power
+
 theor_pow <- 
   ggplot(dat_theor, aes(x = theor_pow, fill = as.factor(eff_size))) + 
   geom_rect(mapping=aes(xmin=0, xmax=0.5, ymin=0, ymax=900),
@@ -130,6 +133,14 @@ theor_pow <-
   ylab("Number of papers") + 
   xlab("Estimated power before experiment") + 
   facet_grid(eff_size ~.) +
+  geom_text(aes(x, y, label=lab, colour = "red"),
+            data = data.frame(x = 0.9, y = 750,
+                              lab = paste0(round(per_power$high_per,1),"%"),
+                              eff_size = all_es),vjust=1) +
+  geom_text(aes(x, y, label=lab, colour = "green"),
+    data=data.frame(x=0.4, y=750,
+                    lab = paste0(round(per_power$low_per,1),"%"),
+                    eff_size = all_es),vjust=1) +
   my_theme + 
   std_fill_dark +
   labs(tag = "Hedge's G") +
@@ -139,47 +150,46 @@ theor_pow <-
         plot.tag.position = c(1.02, 0.535), 
         plot.tag = element_text(angle = -90, size = 10),
         plot.margin = unit(c(0.5,1,0.5,0.5), "cm")) +
-  
   coord_cartesian(clip = "off")
 theor_pow
 
 
 
-## Add in some colors based on the data
-dat_theor$facet_fill_color <- c("my_purple_light", "my_watergreen_light", "my_yellow_light")[as.factor(dat_theor$eff_size)]
-
-## Create main plot
-theor_pow
-
-dummy <- theor_pow
-dummy$layers <- NULL
-dummy <- dummy + new_scale_fill() +
-  # geom_rect(data=dat_theor, xmin=-Inf, ymin=-Inf, xmax=Inf, ymax=Inf,
-  #                          aes(fill = eff_size))
-  geom_rect(data = dat_theor,xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf, aes(fill = facet_fill_color)) +
-  scale_fill_viridis(discrete = TRUE, alpha = 0.01)
-dummy
-
-library(gtable)
-
-g1 <- ggplotGrob(theor_pow)
-g2 <- ggplotGrob(dummy)
-
-
-panels <- grepl(pattern="panel", g2$layout$name)
-strips <- grepl(pattern="strip-right", g2$layout$name)
-g2$grobs[strips] <- replicate(sum(strips), nullGrob(), simplify = FALSE)
-g2$layout$l[panels] <- g2$layout$l[panels] + 1
-g2$layout$r[panels] <- g2$layout$r[panels] + 2
-
-new_strips <- gtable_select(g2, panels | strips)
-grid.newpage()
-grid.draw(new_strips)
-
-
-## ideally you'd remove the old strips, for now they're just covered
-new_plot <- gtable_stack(g1, new_strips)
-grid.draw(new_plot)
+# ## Add in some colors based on the data
+# dat_theor$facet_fill_color <- c("my_purple_light", "my_watergreen_light", "my_yellow_light")[as.factor(dat_theor$eff_size)]
+# 
+# ## Create main plot
+# theor_pow
+# 
+# dummy <- theor_pow
+# dummy$layers <- NULL
+# dummy <- dummy + new_scale_fill() +
+#   # geom_rect(data=dat_theor, xmin=-Inf, ymin=-Inf, xmax=Inf, ymax=Inf,
+#   #                          aes(fill = eff_size))
+#   geom_rect(data = dat_theor,xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf, aes(fill = facet_fill_color)) +
+#   scale_fill_viridis(discrete = TRUE, alpha = 0.01)
+# dummy
+# 
+# library(gtable)
+# 
+# g1 <- ggplotGrob(theor_pow)
+# g2 <- ggplotGrob(dummy)
+# 
+# 
+# panels <- grepl(pattern="panel", g2$layout$name)
+# strips <- grepl(pattern="strip-right", g2$layout$name)
+# g2$grobs[strips] <- replicate(sum(strips), nullGrob(), simplify = FALSE)
+# g2$layout$l[panels] <- g2$layout$l[panels] + 1
+# g2$layout$r[panels] <- g2$layout$r[panels] + 2
+# 
+# new_strips <- gtable_select(g2, panels | strips)
+# grid.newpage()
+# grid.draw(new_strips)
+# 
+# 
+# ## ideally you'd remove the old strips, for now they're just covered
+# new_plot <- gtable_stack(g1, new_strips)
+# grid.draw(new_plot)
 
 # svg(filename = "figures/theoretical_power.svg")
 # grid.draw(new_plot)
@@ -191,7 +201,7 @@ sum_n_meta <- dat %>%
   group_by(study) %>%
   summarize(prior_n_1 = sum(n_1),
             prior_n_3 = sum(n_1) * 0.3)
-dat_theor <- x
+#dat_theor <- x
 dat_theor <- data.frame(dat_theor,
                         sum_n_meta[match(dat_theor$study,
                                          sum_n_meta$study),
@@ -221,30 +231,27 @@ dat_theor$pow_prior <- theor_power$power
 
 # theoretical power visualization
 dat_theor %>%
-  group_by(prior, 
-           eff_size, 
-           select = pow_prior <= 0.5) %>%
-  summarize(amount = length(n_1_c)) %>%
-  spread(select, amount) %>%
-  mutate(per =  (`TRUE` / (`FALSE` + `TRUE`)) * 100) -> low_per_prior
-dat_theor %>%
-  group_by(prior, 
-           eff_size, 
-           select = pow_prior >= 0.8) %>%
-  summarize(amount = length(n_1_c)) %>%
-  spread(select, amount) %>%
-  mutate(per =  (`TRUE` / (`FALSE` + `TRUE`)) * 100) -> high_per_prior
+  group_by(eff_size, prior) %>%
+  summarize(low_per = sum((pow_prior <= 0.5)) /nrow(dat) * 100,
+            high_per = sum((pow_prior >= 0.8))/ nrow(dat) * 100) -> per_power
 
-tryout <- 
-ggplot(dat_theor, aes(x = pow_prior, fill = as.factor(eff_size), alpha = factor(prior))) + 
+
+tryout <-
+ggplot(dat_theor, aes(x = pow_prior, fill = factor(eff_size), alpha = factor(prior))) + 
   geom_rect(mapping=aes(xmin=0, xmax=0.5, ymin=0, ymax=Inf),
-             fill= my_bad) +
+            fill= my_bad) +
   geom_rect(mapping=aes(xmin=0.8, xmax=1, ymin=0, ymax=Inf),
             fill= my_good) +
-  geom_density(colour = "black", bins = 100) +
+  geom_density(data = dat_theor[dat_theor$prior == "n_1",],aes(y = ..scaled..), fill = "grey") +  
+  geom_histogram(data = dat_theor[dat_theor$prior %in% c("n_1_prior_3"),], 
+               #  data = dat_theor[dat_theor$prior %in% c("n_1"),], 
+                                
+                 colour = "black", bins = 100, position = "identity", aes(y = ..ndensity..)) +
+  
   ylab("Number of papers") + 
   xlab("Estimated power before experiment") + 
-  facet_grid(eff_size ~., scale = "free") +
+  facet_grid(eff_size ~.) +
+  scale_y_continuous() +
   my_theme + 
   std_fill_dark +
   labs(tag = "Hedge's G") +
@@ -254,9 +261,6 @@ ggplot(dat_theor, aes(x = pow_prior, fill = as.factor(eff_size), alpha = factor(
         plot.tag.position = c(1.02, 0.535), 
         plot.tag = element_text(angle = -90, size = 10),
         plot.margin = unit(c(0.5,1,0.5,0.5), "cm")) +
-  scale_alpha_discrete(range = c(0.2,0.5,0.8)) + 
+  scale_alpha_discrete(range = c(0.2,0.8)) + 
   coord_cartesian(clip = "off")
 
-ggarrange(
-  
-)
