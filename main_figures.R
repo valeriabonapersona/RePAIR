6530e72594aaa868c4e5ce0ade92f4fdf1bf72a3
valeriabonapersona <valeria.bonapersona@gmail.com>
2019-08-20 12:39:37 +0200

    update figure 2

--- main_figures.R
+++ main_figures.R
@@ -5,9 +5,12 @@ source("RePAIR_functions.R")
 
 
 # Figure 1 ----------------------------------------------------------------
+# import individual images
+pow_ach <- readRDS("figures/power_achieved.rds")
+theor_pow <- readRDS("figures/theoretical_power.rds")
+total_n <- readRDS("figures/total_n.rds")
 
 svg(filename = "figures/Figure_01.svg")
-
 ggarrange(
   
   ggarrange(pow_ach, total_n, ncol = 2, labels = c("A", "B")),
@@ -23,20 +26,20 @@ dev.off()
 
 
 # Figure 2 ----------------------------------------------------------------
-svg(filename = "figures/RePAIR_sim.svg")
+RePAIR_sim <- readRDS("figures/RePAIR_sim.rds")
+theor_pow_prior <- readRDS("figures/theor_pow_prior.rds") ##countMax needs to be saved
+RePAIR_res <- readRDS("figures/repair_results.rds")
 
-  ggarrange(
-    
-    top_left, 
-    top_right,
-    bottom_left,
-    bottom_right,
-    
-    ncol = 2, nrow = 2, 
-    heights = c(1,2), widths = c(1,4)
-    
-  )
+svg(filename = "figures/Figure_02.svg")
+
+ggarrange(
+  RePAIR_sim,
+  
+  ggarrange(theor_pow_prior, RePAIR_res, ncol = 2, labels = c("B", "C"), widths = c(1.5,1)),
+  
+  nrow = 2, 
+  labels = c("A", NA)
+)
 
 dev.off()
-
 
